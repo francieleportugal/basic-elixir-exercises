89639d7d043362f312f26536397800cc63d7fe41
Franciele Portugal <franciele.santos@stone.com.br>
2021-03-22 22:04:21 -0300

    WIP - resolve second exercise

--- lib/solutions.ex
+++ lib/solutions.ex
@@ -16,4 +16,26 @@ defmodule Solutions do
     words_list = String.split(setence)
     Enum.frequencies(words_list)
   end
+
+  @doc """
+  Get two sum index
+
+  ## Examples
+    iex> Solutions.get_two_sum_index([1, 5, 0, 8, 10, 9], 19)
+    [4, 5]
+
+  """
+  def get_two_sum_index(integers_list, target) do
+
+    for x <- integers_list, y <- integers_list, x != y do
+
+      if x + y == target do
+        x_index = Enum.find_index(integers_list, fn item -> item == x end)
+        y_index = Enum.find_index(integers_list, fn item -> item == y end)
+        [x_index, y_index]
+      end
+
+    end
+
+  end
 end
--- test/solutions_test.exs
+++ test/solutions_test.exs
@@ -4,6 +4,9 @@ defmodule SolutionsTest do
 
   test "count words" do
     assert Solutions.get_words_occurrence("turma de elixir formacao elixir de elixir") == %{"de" => 2, "elixir" => 3, "formacao" => 1, "turma" => 1}
+  end
 
+  test "get two sum index" do
+    assert Solutions.get_two_sum_index([1, 5, 0, 8, 10, 9], 19) == [4, 5]
   end
 end
